726d5d774d5b6d1fda94bb59f6aeaaed6d07a4f0
MostafaAbdelAttySultan <89129129+MostafaAbdelAttySultan@users.noreply.github.com>
2022-01-05 00:18:35 +0200

    Create Project.c

---
+++ Project.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+typedef struct{
+  int y;
+  int m;
+  int d;
+}date;
+typedef struct{
+  int Id;
+  char last name[], first name[],the address[],phone number[], email[];
+  float salary;
+  date birth date;
+}empolyee;
+int main ()
+{
+  
+
+  return 0;
+}
